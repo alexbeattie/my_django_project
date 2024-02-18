53cbaf1332f27537637cec33e9981687c3f1c491
Alex Beattie <artisanbranding@me.com>
2024-02-18 09:25:50 -0800

    Initial commit

---
+++ myapp/urls.py
@@ -0,0 +1,13 @@
+# myapp/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ItemViewSet
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r"items", ItemViewSet)
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path("", include(router.urls)),
+]
